f1abb36cb828c053e32924ff9fd9fc9734de97cc
oneofmanyadams <adam.plazewski@gmail.com>
2024-05-05 09:47:25 -0600

    Initial commit, started on PowerZones struct.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module cycling
+
+go 1.22.2
---
+++ power_zones.go
@@ -0,0 +1,9 @@
+package cycling
+
+type PowerZone struct {
+	Number      int
+	Name        string
+	Description string
+	MinWatts    int
+	MaxWatts    int
+}
