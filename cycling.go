e7d0b202b5ed46219be64f087cbadf7f6e98b3b6
oneofmanyadams <adam.plazewski@gmail.com>
2024-05-06 18:46:54 -0600

    Added reference link.

--- cycling.go
+++ cycling.go
@@ -1,3 +1,9 @@
 // Package cycling is designed to prrovide some usefull tools for
 // tracking cycling related information.
 package cycling
+
+////////////////////////////////////////////////////////////////////////////////
+// references:
+
+// Zone details:
+// https://www.trainingpeaks.com/blog/power-training-levels/
