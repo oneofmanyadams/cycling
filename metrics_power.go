b81aadfd558318c984fc333a19d5d60103219bcc
oneofmanyadams <adam.plazewski@gmail.com>
2024-05-06 20:14:56 -0600

    Initial setup of power related metrics.

---
+++ metrics_power.go
@@ -0,0 +1,32 @@
+package cycling
+
+type Session struct {
+	Time         int // in seconds
+	PowerEachSec []int
+	Ftp          int
+}
+
+type TrainingStressScore struct {
+	Time int
+	NP   NormalizedPower
+	IP   IntensityFactor
+	FTP  FunctionalThresholdPower
+}
+
+type NormalizedPower struct {
+	PowerEachSecond []int
+	// Calculate rolling 30 second average power for workout.
+	// Raise resulting values to forth power
+	// Determine average of the ^4 values
+	// Determine 4th root of that average
+}
+
+type IntensityFactor struct {
+	NP  NormalizedPower
+	FTP FunctionalThresholdPower
+	// NP / FTP
+}
+
+type FunctionalThresholdPower struct {
+	// Maximum average power for 1hr
+}
