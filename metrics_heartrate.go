93f7ebc2ef3cd0ebf2491434fcdf2a13ee54442d
oneofmanyadams <adam.plazewski@gmail.com>
2024-05-11 11:52:51 -0600

    Added method for generating new HeartRateMetrics as well as a few initial metrics functions (duplicating logic from power metrics).

--- metrics_heartrate.go
+++ metrics_heartrate.go
@@ -1,6 +1,6 @@
 package cycling
 
-type HearrRateMetrics struct {
+type HeartRateMetrics struct {
 	HeartRateEachSec []int
 	Time             int     // in seconds
 	FTHR             int     // FuntionalThresholdHeartRate
@@ -9,4 +9,34 @@ type HearrRateMetrics struct {
 	VI               float64 // VariabilityIndex
 	IF               float64 // IntensityFactor
 	TSS              float64 // TrainingStressScore
+}
+
+func NewHeartRateMetrics(fthr int, hr_each_second []int) HeartRateMetrics {
+	var hrm HeartRateMetrics
+	hrm.FTHR = fthr
+	hrm.HeartRateEachSec = hr_each_second
+	hrm.CalculateMetrics()
+	return hrm
+}
+
+func (s *HeartRateMetrics) CalculateMetrics() {
+	s.SessionTime()
+	if s.FTHR <= 0 {
+		s.FunctionalThresholdHeartRate()
+	}
+	s.AverageHeartRate()
+}
+
+func (s *HeartRateMetrics) SessionTime() {
+	s.Time = len(s.HeartRateEachSec)
+}
+
+// FunctionalThresholdHeartRate is just a duplication of the formula to
+// calculate FTP, see power metric FunctionalThresholdPower for more info.
+func (s *HeartRateMetrics) FunctionalThresholdHeartRate() {
+	s.FTHR = int(largestSubsetAvg(s.HeartRateEachSec, 1200) * 0.95)
+}
+
+func (s *HeartRateMetrics) AverageHeartRate() {
+	s.AHR = int(avgInts(s.HeartRateEachSec))
 }
