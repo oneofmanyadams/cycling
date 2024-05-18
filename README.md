4756c16b846fd73bb6d122a0fe08c9782aefda30
oneofmanyadams <adam.plazewski@gmail.com>
2024-05-18 16:56:53 -0600

    Added Power Zone calculation function based on rider's FTP and a provided list of power zone's max ftp percentages.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A package for tracking and measuring various cycling related metrics.
 ### ToDo
 - Zones logic
     - ~~PowerZone match based on provided power~~
-    - Zone calculator based on % of FTP
+    - ~~Zone calculator based on % of FTP~~
     - Zone duration calculations?
     - Zone standard templates (which provides %'s)
     - Duplicate logic to HearRate zones
--- zones_power.go
+++ zones_power.go
@@ -3,9 +3,29 @@ package cycling
 import (
 	"errors"
 	"fmt"
+	"sort"
 )
 
 type PowerZones []PowerZone
+
+// NewPowerZones calculates power zones based on a rirder's ftp as well as a
+// list of of each zone's max_ftp (in terrms of a %of the ftp.
+func NewPowerZones(ftp int, max_ftps []float64) PowerZones {
+	sort.Float64s(max_ftps)
+	var pzs PowerZones
+	for len(pzs) < len(max_ftps) {
+		var new_zone PowerZone
+		new_zone.Number = len(pzs) + 1
+		if new_zone.Number == 1 {
+			new_zone.MinWatts = 0
+		} else {
+			new_zone.MinWatts = pzs[len(pzs)-1].MaxWatts + 1
+		}
+		new_zone.MaxWatts = int(float64(ftp) * max_ftps[len(pzs)])
+		pzs = append(pzs, new_zone)
+	}
+	return pzs
+}
 
 func (s *PowerZones) MatchZone(pwr int) (PowerZone, error) {
 	for _, zone := range *s {
