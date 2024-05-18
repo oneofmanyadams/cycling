37415eb569fe8b7503350c0b16d9bf0d9169e560
oneofmanyadams <adam.plazewski@gmail.com>
2024-05-17 22:27:51 -0600

    Added PowerZones type, ability to find a zone based on provided power.

--- zones_power.go
+++ zones_power.go
@@ -1,11 +1,31 @@
 package cycling
 
+import (
+	"errors"
+	"fmt"
+)
+
+type PowerZones []PowerZone
+
+func (s *PowerZones) MatchZone(pwr int) (PowerZone, error) {
+	for _, zone := range *s {
+		if zone.Matches(pwr) {
+			return zone, nil
+		}
+	}
+	return PowerZone{}, errors.New(fmt.Sprintf("No PowerZone for %d", pwr))
+}
+
 type PowerZone struct {
 	Number      int
 	Name        string
 	Description string
 	MinWatts    int
 	MaxWatts    int
+}
+
+func (s *PowerZone) Matches(power int) bool {
+	return (s.MinWatts <= power) && (s.MaxWatts >= power)
 }
 
 func (s *PowerZone) AvgWatts() int {
