b70bd69d7cb2a5a609c94561fef482f7e8c57735
oneofmanyadams <adam.plazewski@gmail.com>
2024-05-21 21:30:36 -0600

    Implemented methods on Zones to fulfill sort module interface.

--- zones.go
+++ zones.go
@@ -2,10 +2,26 @@ package cycling
 
 type Zones []Zone
 
+// Functions to fulfill sort interface.
+func (s Zones) Len() int {
+	return len(s)
+}
+func (s Zones) Less(i, j int) bool {
+	if len(s) <= max(i, j) {
+		return false
+	}
+	return s[i].Level < s[j].Level
+}
+func (s Zones) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
 type Zone struct {
-	Level       int
-	Name        string
-	Description string
-	MinDuration int
-	MaxDuration int
+	Level        int
+	Name         string
+	Description  string
+	MinDuration  int
+	MaxDuration  int
+	MaxHeartRate int
+	MaxPower     int
 }
